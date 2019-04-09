f4a169ec8d63aace8bd55ae0832046eb2dc31630
Bing <39610562+pengzhuobing@users.noreply.github.com>
2019-04-09 10:52:46 +0800

    Update fish_in_winter.pl

--- fish_in_winter.pl
+++ fish_in_winter.pl
@@ -1,12 +1,17 @@
 #!/usr/bin/perl
+#Match the first line of <SEA> according to the first column selected in <FISH>
+
+if($#ARGV<2){
+	die "Usage: perl fish_in_winter.pl <FISH> <SEA> <sel or none> 
+Match the first line of <SEA> according to the first column selected in <FISH> \n"
+}
 open(FISH,$ARGV[0]) or die "$!";
 open(SEA,$ARGV[1]) or die "$!";
 my @fish = <FISH>;
 my @selfish;
 for $i (0..$#fish){
 	@line = split(/\t/,$fish[$i]);
-	if ($line[1]=~/$ARGV[2]/){
-	#$selfish=$selfish.$line[1];
+	if ($line[1]=~/$ARGV[2]/ || !$line[1]){
 	push @selfish,$line[0];
 	print "$line[0]\t";
 	}
@@ -18,7 +23,7 @@ my@head = split(/\t/,$sea[0]);
 for $m (1..$#sea){
 	@txt =split(/\t/,$sea[$m]);
 	for $n(0..$#selfish){
-		if ($head[$m]==$selfish[$n]){
+		if ($head[$m] eq $selfish[$n]){
 			print "$txt[$n]\t";
 		}
 	}
